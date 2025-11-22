7f9c963bf675b8f3fb7dedd38d850df13847ce90
Tatyana Thomas <tatyanathomas@Tatyanas-MBP.lan>
2025-11-22 04:42:36 -0500

    test center pages

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
+from flask import Flask, session, render_template, request, redirect, url_for, jsonify, flash
+from datetime import datetime
 import mysql.connector
 import bcrypt
 import csv
@@ -13,9 +14,9 @@ app.secret_key = "my_se_key"  # allow flash
 def get_connection():
     return mysql.connector.connect(
         host="localhost",
-        user="root",         # XAMPP
-        password="",         # XAMPP
-        database="exam_booker"    # your db name
+        user="root",        
+        password="",         
+        database="exam_booker"    
     )
 
 @app.route('/')
@@ -34,18 +35,22 @@ def login():
         email = request.form.get('username')     # frontend input
         password = request.form.get('password')
         user_type = request.form.get('user_type')  # TT / TC / ES
-
+        print("************ USER TYPE *********** ")
+        print(user_type)
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
+    
 
         # ---- 1) fetch user from DB ----
         cursor.execute("""
-            SELECT user_email, user_password_h, user_type
+            SELECT user_id, user_email, user_password_h, user_type
             FROM user
             WHERE user_email = %s
         """, (email,))
         user = cursor.fetchone()
-
+        user_id = user["user_id"]
+        print("************ USER ID *********** ")
+        print(user_id)
         cursor.close()
         conn.close()
 
@@ -64,12 +69,20 @@ def login():
 
         if bcrypt.checkpw(password.encode('utf-8'), stored_hash):
             flash("Login successful!", "success")
-            return redirect(url_for('home'))
+            if user_type == "TT":
+                session["user_id"] = user_id
+                return redirect(url_for("my_registrations", user_id=user_id))
+            elif user_type == "TC":
+                session["user_id"] = user_id
+                return redirect(url_for("upload_availabilities", user_id=user_id))
+            else:
+                session["user_id"] = user_id
+                return redirect(url_for("offered_exams", user_id=user_id))
+               
         else:
             flash("Incorrect password.", "error")
             return render_template('login.html')
 
-    # GET request → login page
     return render_template('login.html')
 
 
@@ -113,7 +126,7 @@ def register():
 
         flash("Registration successful!", "success")
 
-        # TODO: Redirect based on user type, let the user fill more info regrading their user type
+        # TODO: Redirect based on user type, let the user fill more info regarding their user type
         # TODO: This function cannot insert username and passwords. They need to be filled in on the redirect page
         '''
         if user_type == "TT":
@@ -138,7 +151,6 @@ def view_sponsors():
 @app.route('/availabilities/upload', methods=['GET', 'POST'])
 def upload_availabilities():
     if request.method == 'POST':
-
         # Ensure file exists
         if 'csv_file' not in request.files:
             flash("No file uploaded.", "error")
@@ -154,19 +166,33 @@ def upload_availabilities():
         try:
             conn = get_connection()
             cursor = conn.cursor()
+            user_id = session.get("user_id")
+            cursor.execute("""
+                    SELECT test_center_id FROM test_center
+                    WHERE user_id = %s
+                """, (user_id,))
+            test_center_id = cursor.fetchone()
+            test_center_id = test_center_id[0]
 
             csv_reader = csv.reader(file.stream.read().decode('utf-8').splitlines())
             next(csv_reader)  # skip header
 
             count = 0
             for row in csv_reader:
-                date, start, end, duration, capacity = row
+                str_date, str_start, str_end, duration, str_capacity = row
+    
+                date_of_availability = datetime.strptime(str_date, "%m/%d/%y").strftime("%Y-%m-%d")
+                start_time_slot = datetime.strptime(str_start, "%H:%M").strftime("%H:%M:%S")
+                end_time_slot   = datetime.strptime(str_end, "%H:%M").strftime("%H:%M:%S")
+                
+                print("***********DATA INSERT*************")
+                print(f"{test_center_id}, {date_of_availability}, {start_time_slot}, {end_time_slot}, {int(str_capacity)}")
 
                 cursor.execute("""
                     INSERT INTO test_center_availability 
-                    (date, start_time, end_time, duration_minutes, capacity)
+                    (test_center_id, date_of_availability, start_time_slot, end_time_slot, seat_capacity)
                     VALUES (%s, %s, %s, %s, %s)
-                """, (date, start, end, duration, capacity))
+                """, (test_center_id, date_of_availability, start_time_slot, end_time_slot, int(str_capacity)))
 
                 count += 1
 
@@ -186,28 +212,40 @@ def upload_availabilities():
 
 @app.route('/view_availabilities')
 def view_availabilities():
-    conn = get_connection()
-    cursor = conn.cursor(dictionary=True)
-    # TODO: this just showed everything we have in TC avail table for now
-    cursor.execute("""
-        SELECT 
-            availability_slot_id,
-            test_center_id,
-            date_of_availability,
-            start_time_slot,
-            end_time_slot,
-            seat_capacity,
-            scheduled_count
-        FROM test_center_availability
-        ORDER BY date_of_availability, start_time_slot
-    """)
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
 
-    availabilities = cursor.fetchall()
+        user_id = session.get("user_id")
+        cursor.execute("""
+                SELECT test_center_id FROM test_center
+                WHERE user_id = %s
+            """, (user_id,))
+        test_center_id = cursor.fetchone()
+        test_center_id = test_center_id["test_center_id"]
 
-    cursor.close()
-    conn.close()
+        cursor.execute("""
+            SELECT 
+                date_of_availability,
+                start_time_slot,
+                end_time_slot,
+                seat_capacity,
+                scheduled_count
+            FROM test_centers_with_availability
+            WHERE test_center_id = %s
+            ORDER BY date_of_availability, start_time_slot
+        """,(test_center_id,))
 
-    return render_template('view_availabilities.html', availabilities=availabilities)
+        availabilities = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return render_template('view_availabilities.html', availabilities=availabilities)
+
+    except:
+        return render_template('view_availabilities.html')
+
+
+
 
 
 @app.route('/delete_availability', methods=['POST'])
@@ -232,8 +270,41 @@ def delete_availability():
 
 @app.route('/centers/contract')
 def center_contract():
+
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        user_id = session.get("user_id")
+        cursor.execute("""
+                SELECT test_center_id FROM test_center
+                WHERE user_id = %s
+            """, (user_id,))
+        test_center_id = cursor.fetchone()
+        test_center_id = test_center_id["test_center_id"]
+        print("***********TC ID*************")
+        print("HERE: " , test_center_id)
+
+        cursor.execute("""
+            SELECT test_center_id, center_contract_status, center_start_date, center_end_date, rate_per_seat FROM test_center_contract WHERE test_center_id = %s;
+        """,(test_center_id,))
+
+        contract_details = cursor.fetchall()
+                        
+
+        cursor.close()
+        conn.close()
+        return render_template('center_contract.html', contract_details = contract_details)
+
+    except:
+        return render_template('center_contract.html')
+
+
+@app.route('/my_registrations')
+def my_registrations():
     # TODO
-    return render_template('center_contract.html')
+    return render_template('my_registrations.html')
+
 
 
 
